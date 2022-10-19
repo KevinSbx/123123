b1720898a94eddc812ab4ce29197a95ba7d8aa5c
Y.C Kevin <vocaloid12358@gmail.com>
2022-10-19 13:23:06 +0800

    update1.1

--- app.py
+++ app.py
@@ -38,10 +38,20 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = "感謝您的回復，本功能尚在開發中，敬請期待!!"
+    r = "感謝您的回復，本功能尚在開發中，敬請期待!!"
+
+    if msg in ["hi", "Hi"]:
+        r = "嗨"
+    elif msg == "功能":
+        r = "功能列表正在開發中"
+    elif msg == "你是誰":
+         r ="我是一個正在學習的機器人"
+    elif "預約" in msg:
+        r = "目前預約人數眾多，請稍代回復"
+    
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
